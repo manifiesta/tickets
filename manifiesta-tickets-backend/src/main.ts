79e8bed8dd94785ae793c6fd2aafa3d96403ef0e
FreeCaribou <florian@gerardweb.eu>
2022-10-31 14:24:14 +0100

    fixup! Delete view mode

--- manifiesta-tickets-backend/src/main.ts
+++ manifiesta-tickets-backend/src/main.ts
@@ -1,35 +1,16 @@
-
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
-import * as hbs from 'hbs';
 import { mockingData } from './mocking-data';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  // app.useStaticAssets(join(__dirname, '../src/rendering', 'public'));
-  // app.setBaseViewsDir(join(__dirname, '../src/rendering', 'views'));
-  // hbs.registerPartials(__dirname + '../src/rendering/views/partials');
-
-  // hbs.registerHelper('loud', message => {
-  //   return message.toUpperCase()
-  // });
-
-  // app.setViewEngine('hbs');
-
   await app.listen(3000);
-
-  console.log('environement 1', process.env.NODE_ENV);
 
   if (process.env.NODE_ENV == 'dev' || process.env.NODE_ENV == 'test') {
     await mockingData();
-    console.log('DB op');
   }
 
 }
