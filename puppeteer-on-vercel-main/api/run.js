02f3f2a3f47c24a4bed3806f6c04e14ce215afb2
FreeCaribou <florian@gerardweb.eu>
2023-04-29 21:30:26 +0200

    try vercel 9

--- puppeteer-on-vercel-main/api/run.js
+++ puppeteer-on-vercel-main/api/run.js
@@ -21,39 +21,41 @@ export default async function handler(request, response) {
   const page = await browser.newPage();
 
 
-  await page.goto("https://vercel.com/")
-  const title = await page.title()
-  const screenshot = await page.screenshot({ encoding: 'base64' });
+  // await page.goto("https://vercel.com/")
+  // const title = await page.title()
+  // const screenshot = await page.screenshot({ encoding: 'base64' });
 
   const orderCode = request.url.split('?')[1];
+  const orderUrl = `https://www.vivapayments.com/web2?ref=${orderCode}&paymentmethod=27`;
 
-  // const status = await page.goto(
-  //   `https://www.vivapayments.com/web2?ref=${orderCode}&paymentmethod=27`,
-  // ); // Replace this with the right link.
-  // // await page.waitForTimeout(4000);
-  // const qrCode = await page.$('canvas');
-  // const screenshot = await qrCode.screenshot({ encoding: 'base64' });
-  // response.status(200).json({
-  //   body: request.body,
-  //   // cookies: request.cookies,
-  //   // title,
-  //   // chromium: await chromium.executablePath,
-  //   data: 'data:image/png;base64,' + screenshot,
-  //   url: request.url,
-  //   orderCode: orderCode,
-  // })
-
-
+  const status = await page.goto(orderUrl); // Replace this with the right link.
+  await page.waitForTimeout(6000);
+  const qrCode = await page.$('canvas');
+  const screenshot = await qrCode.screenshot({ encoding: 'base64' });
   await page.close()
   await browser.close()
-
   response.status(200).json({
     body: request.body,
     cookies: request.cookies,
-    title,
     chromium: await chromium.executablePath,
-    data: 'data:image/png;base64,' + screenshot,
     url: request.url,
     orderCode: orderCode,
-  })
+    orderUrl: orderUrl,
+    data: 'data:image/png;base64,' + screenshot,
+  });
+
+
+  // await page.close()
+  // await browser.close()
+
+  // response.status(200).json({
+  //   body: request.body,
+  //   cookies: request.cookies,
+  //   title,
+  //   chromium: await chromium.executablePath,
+  //   data: 'data:image/png;base64,' + screenshot,
+  //   url: request.url,
+  //   orderCode: orderCode,
+  //   orderUrl: orderUrl,
+  // })
 }
