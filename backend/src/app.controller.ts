3f30cef47080586e1106efd5b0bc25c43b6ca03b
FreeCaribou <kingdomflo16@gmail.com>
2024-02-16 17:24:52 +0100

    A route to bypass fucking CORS

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('api')
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello() {
     return this.appService.getHello();
+  }
+
+  @Post('bypass-cors')
+  bypassCors(@Body() url: {url: string}) {
+    return this.appService.bypassCors(url.url);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,16 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  constructor(private httpService: HttpService) {}
+
   getHello() {
     return {version: '0.0.1', name: 'manifiesta-tickets'};
+  }
+
+  bypassCors(url: string) {
+    return this.httpService.get(url);
   }
 }
